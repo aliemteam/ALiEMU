1756a80d8a29686c79ed91c106962aa894025a4f
cgaafary <cgaafary@gmail.com>
2016-06-16 14:17:38 -0400

    Fix Div close take

--- wp-content/themes/Divi-child/page-home.php
+++ wp-content/themes/Divi-child/page-home.php
@@ -30,17 +30,18 @@
                             <div class="au-info-card">
                                 <div class="au-info-header"><h1>What is ALiEMU?</h1></div>
                                 <div class="au-info-text"><p>ALiEMU can best be thought of as our open-access, on-demand, online school of e-courses for anyone practicing Emergency Medicine worldwide. We ultimately envision serving as a platform where educators can submit e-courses, whereupon each will undergo expert peer review and receive instructional design assistance to optimize online content delivery and learner retention. Only high-quality, vetted courses relevant to the EM community will be published as an on-demand course on ALiEMU.</p></div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <!-- <div class="au-sponsors">
+                <div class="au-sponsors">
                     <div>
                         <a href="http://www.cordem.org/" target="_blank"><img src="https://www.aliemu.com/wp-content/uploads/2016/06/CORD-EM-transp.png"></a>
                     </div>
                     <div>
                         <a href="http://www.theteachingcourse.com/" target="_blank"><img src="https://www.aliemu.com/wp-content/uploads/2016/06/Teaching-Course-Logo.png"></a>
                     </div>
-                </div> -->
+                </div>
             </div>
         </article>
     <?php endwhile; ?>
