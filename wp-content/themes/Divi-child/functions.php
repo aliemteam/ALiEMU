90a79b0d70284741e342cfa3fc87406dda0360b5
Derek P Sifford <dereksifford@gmail.com>
2016-06-18 18:51:36 -0400

    Remove emojis

--- wp-content/themes/Divi-child/functions.php
+++ wp-content/themes/Divi-child/functions.php
@@ -1,6 +1,12 @@
 <?php
 
 /**
+ * Remove emojis
+ */
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+
+/**
  * Shortcode which GETs and returns the recommended III hours for certificates.
  * @return string The recommended hours
  */
