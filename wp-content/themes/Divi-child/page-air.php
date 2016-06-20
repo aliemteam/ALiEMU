881e1ad94e40856843c2c68a0f0d55d2a00ec1a4
cgaafary <cgaafary@gmail.com>
2016-06-20 12:04:39 -0400

    More mobile styling tweaks

--- wp-content/themes/Divi-child/page-air.php
+++ wp-content/themes/Divi-child/page-air.php
@@ -4,16 +4,13 @@
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
             <div class="au-container">
     <div class="au-heading">
-      <h1>ALiEM Approved Instructional Resources (AIR)</h1>
+          <h1>ALiEM Approved Instructional Resources (AIR)</h1>
     </div>
     <div class="au-content">
       <div class="au-text">
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae hic minima itaque suscipit neque ullam, necessitatibus iusto ab, atque quam impedit recusandae. Ut quisquam neque eius sint? Commodi, voluptate, earum.</div>
-        <div>Consequuntur perspiciatis enim voluptatum cum delectus nostrum quos cumque corporis, fuga. Deserunt, dignissimos pariatur velit numquam! Accusamus repellendus itaque deleniti quos quisquam minus quibusdam incidunt, mollitia obcaecati! Natus, quae minima.</div>
-        <div>Perferendis amet cupiditate aperiam sequi omnis vero nemo rem cumque iste possimus aut molestiae a harum maxime eos, est voluptate id mollitia deleniti, beatae veritatis accusamus. Tenetur reprehenderit, laudantium nobis.</div>
-        <div>Aut excepturi assumenda deleniti ea nemo hic quia eius nesciunt adipisci, temporibus, nisi vitae voluptas quam animi facere maxime quod dolores aliquam odio consectetur est placeat modi. Illo, dicta quaerat!</div>
+        <div>Welcome to the Approved Instructional Resources (AIR) series! This series screens and grades open access blog posts and podcasts in the field of emergency medicine (EM) over the past 12 months to identify high quality, social media-based, educational resources for EM residents. This series is one of the most popular initiatives used by over 80 U.S. EM residency programs to fulfill their Individualized Interactive Instruction (III), or asynchronous online learning, conference needs. Below are the AIR blocks listed in reverse chronological order along with their suggested number of III conference hour credit. If you are a program director and wish to track your residents’ progress, please sign up for our Educator Dashboard access.</div>
       </div>
-      <div class="au-image"><img src="http://www.placehold.it/400x400"/></div>
+      <div class="au-image"><img src="https://www.aliemu.com/wp-content/uploads/2016/06/air-badge.png"/></div>
     </div>
   </div>
   <div class="links-row">
