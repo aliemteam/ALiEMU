7d57b5101e2d482cabfb2f92ac10051fe45ee6b0
cgaafary <cgaafary@gmail.com>
2016-06-21 20:17:13 -0400

    Adjusting faculty-start-page markup

--- wp-content/themes/Divi-child/page-faculty-start.php
+++ wp-content/themes/Divi-child/page-faculty-start.php
@@ -8,19 +8,7 @@
                     <div class="au-container">
                         <div class="au-info-card">
                             <div class="au-info-header"><h1>Attention</h1></div>
-                            <div class="au-info-text"><p>ALiEMU can best be thought of as our open-access, on-demand, online school of e-courses for anyone practicing Emergency Medicine worldwide. We ultimately envision serving as a platform where educators can submit e-courses, whereupon each will undergo expert peer review and receive instructional design assistance to optimize online content delivery and learner retention. Only high-quality, vetted courses relevant to the EM community will be published as an on-demand course on ALiEMU.</p></div>
-                        </div>
-                    </div>
-                </div>
-                <div id="row-3">
-                    <div class="au-container">
-                        <div class="au-info-card">
-                            <div class="au-info-header"><h1>Click a series to learn more</h1></div>
-                            <div class="au-series-logos">
-                                <div><a href="/air"><img src="/wp-content/uploads/2016/06/air-button.png"></a></div>
-                                <div><a href="/air-pro"><img src="/wp-content/uploads/2016/06/airpro-button.png"></a></div>
-                                <div><a href="/capsules"><img src="/wp-content/uploads/2016/06/capsules-button.png"></a></div>
-                            </div>
+                            <div class="au-info-text"><p>This page is intended for those with faculty appointments at U.S. Emergency Medicine residency programs. We welcome all learners to use and learn from ALiEMU, however we have built features that are only intended for U.S. Emergency Medicine faculty accounts. If you are are not a faculty representative at a U.S. Emergency Medicine residency program, then <a href="">click here</a> to return to the general registration page. We hope to offer our special educator features to international residency programs in the future.</p></div>
                         </div>
                     </div>
                 </div>
