7e37a306d51562372b77e27dd34ba33531d09d0d
Derek P Sifford <dereksifford@gmail.com>
2016-07-18 13:01:40 -0400

    Various tweaks...
    - Move coverage dir to new 'tmp' dir and adjust travis accordingly.
    - Adjust 'staging' compose file for emintro domain.
    - Heavily refactor gulpfile for readability.
    - Add SITE_NAME variable to .env file so it can be used in various
      locations within the docker-compose files.
    - Fix up deploy script.
    - Add prompt in server-deploy script for SITE_NAME.
    - Switch server-deploy script to digitalocean nyc1 region so we can use
      block storage.
    - Add nodejs and npm in server so we can run one-off commands to fix
      dependencies.
    - Add csso to devDependencies for minifying dependency files.
    - Add merge-stream to devDependencies so gulpfile could be condensed and
      more readable.
    - Add 'minify' script which should **only be ran on the server**.

--- gulpfile.js
+++ gulpfile.js
@@ -8,33 +8,65 @@ const del = require('del');
 const exec = require('child_process').exec;
 const replace = require('gulp-replace');
 const rename = require('gulp-rename');
-const jade = require('gulp-jade2php');
+const merge = require('merge-stream');
 // Assets
 const svgmin = require('gulp-svgmin');
+// PHP
+const jade = require('gulp-jade2php');
 // CSS
 const stylus = require('gulp-stylus');
 const poststylus = require('poststylus');
 const autoprefixer = require('autoprefixer')({ browsers: ['last 2 versions'] });
 const rucksack = require('rucksack-css');
 const sourcemaps = require('gulp-sourcemaps');
+const csso = require('gulp-csso'); // used for Divi and Learndash minification
 // JS
 const uglify = require('gulp-uglify');
 // TypeScript
 const webpack = require('webpack-stream');
+
+
+// ==================================================
+//                Configurations
+// ==================================================
+
 const webpackConfig = require('./webpack.config.js');
 const webpackDevConfig = Object.assign({}, webpackConfig, {
     devtool: 'eval-source-map',
     cache: true,
 });
 
+const jadeConfig = {
+    omitPhpRuntime: true,
+    omitPhpExtractor: true,
+    arraysOnly: false,
+    noArraysOnly: true,
+};
 
+const stylusConfig = {
+    use: [
+        poststylus([
+            rucksack,
+            autoprefixer,
+        ]),
+    ],
+};
+
+const uglifyConfig  = {
+    compress: {
+        'dead_code': true,
+        'unused': true,
+        'drop_debugger': true,
+        'drop_console': true,
+    },
+};
 
 // ==================================================
 //                 Utility Tasks
 // ==================================================
 
 // Delete all files in dist/Divi-child/
-gulp.task('clean', (done) => del(['dist/Divi-child/**/*'], done) );
+gulp.task('clean', (done) => del(['dist/Divi-child/**/*'], done));
 
 // Take ownership of dist directory
 gulp.task('chown', (done) => {
@@ -54,120 +86,71 @@ gulp.task('reload', (done) => {
     done();
 });
 
-// ==================================================
-//                    Assets
-// ==================================================
-
-gulp.task('assets', () =>
-    gulp.src(
-        'aliemu/assets/**.*',
-        { base: './aliemu'}
-    )
-    .pipe(svgmin())
-    .pipe(gulp.dest('dist/Divi-child'))
-);
 
 // ==================================================
 //                    Static
 // ==================================================
 
-gulp.task('static:main', () =>
-    gulp.src([
-        'aliemu/**/*.*',
-        '!aliemu/templates/**/*.*',
-        '!aliemu/templates',
-        '!aliemu/**/__tests__',
-        '!aliemu/**/*.{ts,tsx,json,styl,md,txt}',
-    ], { base: './aliemu' })
-    .pipe(gulp.dest('dist/Divi-child'))
-);
+gulp.task('static', () => {
 
-gulp.task('static:templates:pages', () =>
-    gulp.src([
-        'aliemu/templates/pages/*.jade'
-    ], { base: './aliemu/templates/pages' })
-    .pipe(jade({
-        omitPhpRuntime: true,
-        omitPhpExtractor: true,
-        arraysOnly: false,
-        noArraysOnly: true,
-    }))
-    .pipe(rename({
-        extname: '.php',
-        prefix: 'page-'
-    }))
-    .pipe(gulp.dest('dist/Divi-child/'))
-);
+    const assets = gukp
+        .src('aliemu/assets/**.*', { base: './aliemu' })
+        .pipe(svgmin())
+        .pipe(gulp.dest('dist/Divi-child'));
 
-gulp.task('static:templates:overrides', () =>
-    gulp.src([
-        'aliemu/templates/overrides/*.jade'
-    ], { base: './aliemu/templates/overrides'})
-    .pipe(jade({
-        omitPhpRuntime: true,
-        omitPhpExtractor: true,
-        arraysOnly: false,
-        noArraysOnly: true,
-    }))
-    .pipe(rename({
-        extname: '.php',
-    }))
-    .pipe(gulp.dest('dist/Divi-child/'))
-);
+    const main = gulp
+        .src([
+            'aliemu/**/*.*',
+            '!aliemu/templates/**/*.*',
+            '!aliemu/templates',
+            '!aliemu/**/__tests__',
+            '!aliemu/**/*.{ts,tsx,json,styl,md,txt}',
+        ], { base: './aliemu' })
+        .pipe(gulp.dest('dist/Divi-child'));
 
-gulp.task('static:learndash', () =>
-    gulp.src([
-        'aliemu/templates/learndash/*.jade'
-    ], { base: './aliemu/templates'})
-    .pipe(jade({
-        omitPhpRuntime: true,
-        omitPhpExtractor: true,
-        arraysOnly: false,
-        noArraysOnly: true,
-    }))
-    .pipe(rename({
-        extname: '.php',
-    }))
-    .pipe(gulp.dest('dist/Divi-child/'))
-);
+    const templatePages = gulp
+        .src('aliemu/templates/pages/*.jade', { base: './aliemu/templates/pages' })
+        .pipe(jade(jadeConfig))
+        .pipe(rename({ extname: '.php', prefix: 'page-'}))
+        .pipe(gulp.dest('dist/Divi-child/'));
 
-gulp.task('static',
-    gulp.parallel(
-        'static:main',
-        'static:templates:pages',
-        'static:templates:overrides',
-        'static:learndash'
-    )
-);
+    const templateOverrides = gulp
+        .src('aliemu/templates/overrides/*.jade', { base: './aliemu/templates/overrides'})
+        .pipe(jade(jadeConfig))
+        .pipe(rename({ extname: '.php' }))
+        .pipe(gulp.dest('dist/Divi-child/'));
+
+    const templateLearndash = gulp
+        .src('aliemu/templates/learndash/*.jade', { base: './aliemu/templates' })
+        .pipe(jade(jadeConfig))
+        .pipe(rename({ extname: '.php' }))
+        .pipe(gulp.dest('dist/Divi-child/'));
+
+    return merge(assets, main, templatePages, templateOverrides, templateLearndash);
+});
 
 // ==================================================
 //                     Styles
 // ==================================================
 
 gulp.task('stylus:dev:step1', () =>
-    gulp.src([
-        'aliemu/styles/style.styl',
-    ], { base: './aliemu/styles', })
-    .pipe(sourcemaps.init())
-    .pipe(stylus({
-        use: [ poststylus([rucksack, autoprefixer]), ]
-    }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist/Divi-child'))
-    .pipe(browserSync.stream({ match: '**/*.css' }))
+    gulp
+        .src('aliemu/styles/style.styl', { base: './aliemu/styles' })
+        .pipe(sourcemaps.init())
+        .pipe(stylus(stylusConfig))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist/Divi-child'))
+        .pipe(browserSync.stream({ match: '**/*.css' }))
 );
 
 gulp.task('stylus:prod:step1', () =>
-    gulp.src([
-        'aliemu/styles/style.styl',
-    ], { base: './aliemu/styles', })
-    .pipe(stylus({
-        use: [ poststylus([rucksack, autoprefixer]), ],
-        compress: true,
-    }))
-    .pipe(gulp.dest('dist/Divi-child'))
+    gulp
+        .src('aliemu/styles/style.styl', { base: './aliemu/styles' })
+        .pipe(stylus(Object.assign({}, stylusConfig, { compress: true })))
+        .pipe(gulp.dest('dist/Divi-child'))
 );
 
+// FIXME: We can probably pitch this
 gulp.task('stylus:step2', (done) => {
     const header =
     '/*\n' +
@@ -208,18 +191,9 @@ gulp.task('webpack:prod', () =>
 );
 
 gulp.task('js', () =>
-    gulp.src([
-        'dist/**/*.js',
-    ])
-    .pipe(uglify({
-        compress: {
-            'dead_code': true,
-            'unused': true,
-            'drop_debugger': true,
-            'drop_console': true,
-        }
-    }))
-    .pipe(gulp.dest('dist'))
+    gulp.src('dist/**/*.js', { base: './' })
+    .pipe(uglify(uglifyConfig))
+    .pipe(gulp.dest('./'))
 );
 
 // ==================================================
@@ -228,7 +202,7 @@ gulp.task('js', () =>
 
 gulp.task('build', gulp.series(
     'clean',
-    gulp.parallel('static', 'assets', 'webpack:prod', 'stylus:prod'),
+    gulp.parallel('static', 'webpack:prod', 'stylus:prod'),
     'js'
 ));
 
@@ -236,20 +210,11 @@ gulp.task('default',
     gulp.series(
         'chown',
         'clean',
-        gulp.parallel('static', 'assets', 'webpack:dev', 'stylus:dev'), () => {
+        gulp.parallel('static', 'webpack:dev', 'stylus:dev'), () => {
 
-            // Tries to connect via docker-machine first, if it fails, then default to
-            // docker native (keeping this in the event we get more contributors in the future)
-            try {
-                let MACHINE_IP = process.env.DOCKER_HOST.match(/\d.*(?=:)/)[0] + ':8080';
-                browserSync.init({
-                    proxy: MACHINE_IP
-                });
-            } catch(e) {
-                browserSync.init({
-                    proxy: 'localhost:8080'
-                });
-            }
+            browserSync.init({
+                proxy: 'localhost:8080'
+            });
 
             gulp.watch([
                 'aliemu/**/*.styl',
@@ -289,19 +254,30 @@ gulp.task('fix-divi', () => {
     ]);
     `;
 
-    return gulp.src([
-        './wp-content/themes/Divi/includes/builder/main-modules.php'
-    ], { base: './' })
-    .pipe(replace(/wp_mail(.|\n)+?;/, slackEmailHook))
-    .pipe(gulp.dest('./'));
+    const slack = gulp
+        .src('wp-content/themes/Divi/includes/builder/main-modules.php', { base: './' })
+        .pipe(replace(/wp_mail(.|\n)+?;/, slackEmailHook))
+        .pipe(gulp.dest('./'));
+
+    const js = gulp
+        .src('wp-content/themes/Divi/**/*.js', { base: './' })
+        .pipe(uglify(uglifyConfig))
+        .pipe(gulp.dest('./'))
+
+    const css = gulp
+        .src('wp-content/themes/Divi/**/*.css', { base: './' })
+        .pipe(csso())
+        .pipe(gulp.dest('./'))
+
+    return merge(slack, js, css);
+
 });
 
 gulp.task('fix-learndash', () =>
-    gulp.src([
-        './wp-content/plugins/sfwd-lms/**/*.{php,po,pot}'
-    ], { base: './' })
-    .pipe(replace(/PRINT YOUR CERTIFICATE!?/, 'Print Certificate'))
-    .pipe(gulp.dest('./'))
+    gulp
+        .src('wp-content/plugins/sfwd-lms/**/*.{php,po,pot}', { base: './' })
+        .pipe(replace(/PRINT YOUR CERTIFICATE!?/, 'Print Certificate'))
+        .pipe(gulp.dest('./'))
 );
 
 
