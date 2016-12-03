40c191b3700e3fc6f6afceb3f837470aad51cbb5
Derek P Sifford <dereksifford@gmail.com>
2016-12-02 23:55:54 -0500

    Add course progress tab back to profile pages

--- aliemu/features/dashboards/index.php
+++ aliemu/features/dashboards/index.php
@@ -1,35 +1,37 @@
 <?php
-/**
- * Adds Educator Dashboard Functionality
- *
- * @since 1.0.0
- * @version 0.0.2
- * @author Chris Gaafary
- */
 
- /***********************************************************
-  *     Ultmate Member Educator Dashboard Functionality     *
-  ***********************************************************/
-
-// Add Educator Dashboard Tab
-add_filter('um_profile_tabs', 'edudash_tab', 1000);
-function edudash_tab( $tabs ) {
+function init_custom_tabs($tabs) {
     $meta = get_user_meta(get_current_user_id(), "residency_us_em", true);
+
+    // Course Progress Tab -- Show for all users.
+    $tabs['progress'] = [
+        'name' => 'My Progress',
+        'icon' => 'um-faicon-bar-chart',
+        'custom' => true,
+    ];
+
+    // Educator Dashboard Tab -- Only show if user has "educator_access" role
     $hasEducatorAccess = !empty($meta) && current_user_can('educator_access');
     if (current_user_can('administrator') || $hasEducatorAccess) {
         $tabs['edudash'] = [
             'name' => 'Educator Dashboard',
             'icon' => 'um-faicon-bar-chart',
-            'custom' => true
+            'custom' => true,
         ];
     }
+
     return $tabs;
 }
+add_filter('um_profile_tabs', 'init_custom_tabs', 1000);
 
- // Tell the tab what to display
- add_action('um_profile_content_edudash_default', 'um_profile_content_edudash_default');
- function um_profile_content_edudash_default() {
+// Educator dashboard tab
+function render_educator_dashboard_tab() {
     require_once(dirname(__FILE__) . '/educator-dashboard/index.php');
- }
+}
+add_action('um_profile_content_edudash', 'render_educator_dashboard_tab');
 
- ?>
+// User course progress tab
+function render_user_progress_tab() {
+    echo do_shortcode('[ld_profile]');
+}
+add_action('um_profile_content_progress', 'render_user_progress_tab');
