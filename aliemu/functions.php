6babd859cc8e916ba7cb338a78ca968fe272ebd7
Derek P Sifford <dereksifford@gmail.com>
2017-06-16 16:25:46 -0400

    initiate semver + bump to version 1.2.0

--- aliemu/functions.php
+++ aliemu/functions.php
@@ -4,7 +4,7 @@
  * Globals
  */
 $ROOT_URI = get_stylesheet_directory_uri();
-define('ALIEMU_VERSION', '1.1.0');
+define('ALIEMU_VERSION', '1.2.0');
 
 require_once(dirname(__FILE__) . '/features/index.php');
 require_once(dirname(__FILE__) . '/functions/index.php');
