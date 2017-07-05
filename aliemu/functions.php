8215848d6c0c5dbf7b0bd4dfe3e8998958b19ef9
Derek P Sifford <dereksifford@gmail.com>
2017-07-05 13:23:45 -0400

    version bump

--- aliemu/functions.php
+++ aliemu/functions.php
@@ -7,7 +7,7 @@ if (!defined('ABSPATH')) exit(1);
 /**
  * Globals
  */
-define('ALIEMU_VERSION', '1.3.1');
+define('ALIEMU_VERSION', '1.3.2');
 define('ROOT_URI', get_stylesheet_directory_uri());
 
 require_once(__DIR__ . '/lib/script-loader/index.php');
