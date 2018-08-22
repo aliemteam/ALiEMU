5ca5ac2c8265cd0a243432203cb6adec5633fb9f
Derek P Sifford <dereksifford@gmail.com>
2018-08-22 19:35:27 -0400

    fix textarea focus state

--- lib/types/window.d.ts
+++ lib/types/window.d.ts
@@ -4,12 +4,11 @@ declare global {
     interface Window {
         readonly AU_AJAX: {
             readonly nonce: string;
+            readonly url: string;
         };
         readonly AU_API: {
             readonly nonce: string;
             readonly url: string;
         };
-        readonly AU_SESSION_TOKEN: string;
-        readonly ajaxurl: string;
     }
 }
--- src/js/components/forms/textarea.tsx
+++ src/js/components/forms/textarea.tsx
@@ -1,14 +1,22 @@
 import classNames from 'classnames';
 import React, { HTMLProps, PureComponent } from 'react';
 
+import { MaybeLabel } from './label';
+
 import styles from './textarea.scss';
 
-type Props = HTMLProps<HTMLTextAreaElement>;
+interface Props extends HTMLProps<HTMLTextAreaElement> {
+    label?: string;
+}
 
 export default class TextArea extends PureComponent<Props> {
     render(): JSX.Element {
-        const { className, ...props } = this.props;
+        const { className, label, ...props } = this.props;
         const classname = classNames(className, styles.textarea);
-        return <textarea {...props} className={classname} />;
+        return (
+            <MaybeLabel label={label} disabled={props.disabled}>
+                <textarea {...props} className={classname} />
+            </MaybeLabel>
+        );
     }
 }
