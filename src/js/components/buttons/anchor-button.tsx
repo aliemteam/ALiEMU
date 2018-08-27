30684ea7f2cfb16d600f722659a65780c7447543
Derek P Sifford <dereksifford@gmail.com>
2018-08-27 16:44:44 -0400

    fix disabled state of anchor-button
    Closes #256

--- src/js/components/buttons/anchor-button.tsx
+++ src/js/components/buttons/anchor-button.tsx
@@ -1,18 +1,36 @@
 import classNames from 'classnames';
-import React, { HTMLProps, PureComponent } from 'react';
+import React, { HTMLProps, MouseEvent, PureComponent } from 'react';
 
 import styles from './anchor-button.scss';
 
 interface Props extends HTMLProps<HTMLAnchorElement> {}
 
 export default class Button extends PureComponent<Props> {
+    static defaultProps = {
+        onClick: () => void 0,
+    };
     render(): JSX.Element {
-        const { children, className, ...btnProps } = this.props;
-        const classname = classNames(styles.btn, className);
+        const { children, className, disabled, ...btnProps } = this.props;
+        const classname = classNames(styles.btn, className, {
+            [styles.disabled]: disabled,
+        });
         return (
-            <a {...btnProps} className={classname} role="button" tabIndex={0}>
+            <a
+                {...btnProps}
+                role="button"
+                tabIndex={disabled ? -1 : 0}
+                className={classname}
+                onClick={this.handleClick}
+            >
                 {children}
             </a>
         );
     }
+
+    private handleClick = (e: MouseEvent<HTMLAnchorElement>): void => {
+        const { disabled, onClick } = this.props;
+        if (!disabled) {
+            onClick!(e);
+        }
+    };
 }
